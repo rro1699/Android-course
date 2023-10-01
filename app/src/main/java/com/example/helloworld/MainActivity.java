b985c5a2b2a17275fa78ab239e173806a9fd3344
Roman Rachugin <rro1699@mail.ru>
2023-10-01 21:26:40 +0300

    added button and logic for send http request on local server by specific IP-address

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
@@ -21,6 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Button button = this.findViewById(R.id.button);
+
+        // operations to be performed
+        // when user tap on the button
+        if (button != null) {
+            button.setOnClickListener(it -> {
+                String url = "http://10.0.2.2:8090/and";
+                System.out.println("try to send request by url = "+url);
+                Request request = new Request.Builder()
+                        .url(url)
+                        .get()
+                        .build();
+                try {
+                    Response execute = client.newCall(request).execute();
+                    System.out.println(execute.body() + "; code = " + execute.code());
+                } catch (IOException e) {
+                    System.out.println("Bad response. Some errors");
+                    throw new RuntimeException(e);
+                }
+            });
+        }
     }
 
 
