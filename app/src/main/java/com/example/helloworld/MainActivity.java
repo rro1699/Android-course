2612b792588eb36e5b96f0b7f19181f81a997700
Roman Rachugin <rro1699@mail.ru>
2023-10-17 21:13:42 +0300

    added examples of notification

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,27 +1,47 @@
 package com.example.helloworld;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-
-import java.io.IOException;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final OkHttpClient client = new OkHttpClient();
-
-
+    private int state = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                int lengthLong;
+                String text;
+                if(state == 0 ){
+                    lengthLong = Toast.LENGTH_SHORT;
+                    text = "LENGTH_SHORT";
+                    state = 1;
+                }else{
+                    lengthLong = Toast.LENGTH_LONG;
+                    text = "LENGTH_LONG";
+                    state = 0;
+                }
+                Toast toast =
+                        Toast.makeText(context,text, lengthLong);
+                toast.show();
+            }
+        });
     }
+
+
 
 
 }
