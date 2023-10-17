aacc1889bddb466d6483f7dd25606c04b244c38d
Roman Rachugin <rro1699@mail.ru>
2023-10-17 21:34:59 +0300

    added examples of included layouts

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int clicks = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        View plusButtonView = findViewById(R.id.plus_button);
+        View minusButtonView = findViewById(R.id.minus_button);
+        TextView clicksText = findViewById(R.id.clicksText);
+
+        Button plusButton = plusButtonView.findViewById(R.id.clickBtn);
+        Button minusButton = minusButtonView.findViewById(R.id.clickBtn);
+
+        plusButton.setText("+");
+        minusButton.setText("-");// почему это тут?
+
+        plusButton.setOnClickListener(v -> {
+            clicks++;
+            clicksText.setText(clicks + " Clicks");
+        });
+        minusButton.setOnClickListener(v -> {
+            clicks--;
+            clicksText.setText(clicks + " Clicks");
+        });
+    }
 
 }
