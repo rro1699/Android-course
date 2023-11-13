fe65558ecc9860fe74dc57dfc0d91226ffb528f2
Roman Rachugin <rro1699@mail.ru>
2023-11-13 20:10:06 +0300

    sensor examples

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,26 +2,68 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 
-import java.io.IOException;
+/*import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;*/
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final OkHttpClient client = new OkHttpClient();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = manager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensorList) {
+            Log.d("MainActivity",sensor.getName());
+        }
+        manager.registerListener(listener,manager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE),SensorManager.SENSOR_DELAY_GAME);
+
+        /*ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();*/
+
     }
 
+    private final SensorEventListener listener = new SensorEventListener() {
+        private final List<Integer> TYPES = List.of(Sensor.TYPE_AMBIENT_TEMPERATURE,
+                Sensor.TYPE_PROXIMITY,Sensor.TYPE_PRESSURE,Sensor.TYPE_LIGHT,Sensor.TYPE_RELATIVE_HUMIDITY);
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if(TYPES.contains(sensorEvent.sensor.getType())){
+                Log.d(sensorEvent.sensor.getName(), ""+sensorEvent.values[0]);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
 
 }
